b74ea9dbae64c39867ddd73e395bd4568f022c2c
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-02-13 23:33:37 +0500

    Вывод фильтров категории.

--- e-commerce-vue-js-app/src/views/Category.vue
+++ e-commerce-vue-js-app/src/views/Category.vue
@@ -23,10 +23,68 @@
     <template v-for="tag in tagList">
       <a-tag :key="tag.link">{{ tag.name }}</a-tag>
     </template>
+    <a-layout>
+      <a-layout-sider width="300" style="background: #fff">
+        <a-collapse>
+          <a-collapse-panel header="Категории">
+            <a-tree :treeData="categories">
+              <template slot="title" slot-scope="{ url, title }">
+                <router-link :to="{ name: 'Category', params: { url: url } }">
+                  <strong>{{ title }}</strong>
+                </router-link>
+              </template>
+            </a-tree>
+          </a-collapse-panel>
+          <template v-for="filter in filters">
+            <template v-if="filter.ftype === 'RESPONSE_FILTER_TYPE_BOOL'">
+              <a-collapse-panel :key="filter.key" :header="filter.name">
+                <a-switch :defaultChecked="filter.values[0].value" />
+              </a-collapse-panel>
+            </template>
+            <template v-else-if="filter.ftype === 'RESPONSE_FILTER_TYPE_RADIO'">
+              <a-collapse-panel :key="filter.key" :header="filter.name">
+                <a-radio-group :name="filter.key" :default-value="1">
+                  <template v-for="value in filter.values">
+                    <a-radio :key="value.key" :value="value.key">{{ value.value }}</a-radio>
+                  </template>
+                </a-radio-group>
+              </a-collapse-panel>
+            </template>
+            <template v-else-if="filter.ftype === 'RESPONSE_FILTER_TYPE_RANGE'">
+              <a-collapse-panel :key="filter.key" :header="filter.name">
+                <a-slider range :default-value="[filter.values[0].range.from, filter.values[0].range.to]" :min="filter.values[0].range.min" :max="filter.values[0].range.max" :step="filter.values[0].range.step" />
+                <a-row>
+                  <a-col :span="8">
+                    <a-input-number :value="filter.values[0].range.from" :min="filter.values[0].range.min" :max="filter.values[0].range.max" :step="filter.values[0].range.step" />
+                  </a-col>
+                  <a-col :span="8">
+                    
+                  </a-col>
+                  <a-col :span="8">
+                    <a-input-number :value="filter.values[0].range.to" :min="filter.values[0].range.min" :max="filter.values[0].range.max" :step="filter.values[0].range.step" />
+                  </a-col>
+                </a-row>
+              </a-collapse-panel>
+            </template>
+            <template v-else-if="filter.ftype === 'RESPONSE_FILTER_TYPE_MULTI'">
+              <a-collapse-panel :key="filter.key" :header="filter.name">
+                <template v-for="value in filter.values">
+                  <a-checkbox :key="value.key" :value="value.value">{{ value.value }}</a-checkbox>
+                </template>
+              </a-collapse-panel>
+            </template>
+            <template v-else>
+              {{ filter.ftype }}
+            </template>
+          </template>
+        </a-collapse>
+      </a-layout-sider>
+    </a-layout>
   </div>
 </template>
 
 <script>
+  import { map as _map } from 'lodash'
   import API from '@/api'
   import { getWidget } from '@/services/utils/widgetsUtils'
 
@@ -36,7 +94,8 @@
     data: () => ({
       isLoading: false,
       category: null,
-      listing: []
+      listing: [],
+      activeKey: 1,
     }),
 
     watch: {
@@ -84,17 +143,42 @@
       catalogResultsHeader() {
         return getWidget(this.widgets, 'catalogResultsHeader')
       },
+      searchResultsFilters() {
+        return getWidget(this.widgets, 'searchResultsFilters')
+      },
       totalFound() {
         return this.catalogResultsHeader?.totalFound || '0 товаров'
       },
       tagList() {
         return getWidget(this.widgets, 'tagList')?.items || []
       },
+      categories() {
+        return this.mapCategories(this.searchResultsFilters?.categories)
+      },
+      filters() {
+        console.log(this.searchResultsFilters?.filters)
+        return this.searchResultsFilters?.filters || []
+      },
     },
 
     methods: {
       async loadCategory() {
         this.category = await API.catalog.getCatalog(this.url, 1)
+      },
+      mapCategories(categories){
+        if(categories){
+          if(Array.isArray(categories)){
+            return _map(categories, (category) => { 
+              return {
+                url: category.info.overrideUrl || `/category/${category.info.urlValue}`,
+                title: category.info.name,
+                scopedSlots: { title: 'title' },
+                children: this.mapCategories(category.categories)
+              }
+            })
+          }
+        }
+        return []
       },
     }
   }
