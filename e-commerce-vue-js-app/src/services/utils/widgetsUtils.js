119d91e7f34fefa46f73641fd2b02bf96231bf9e
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-02-19 11:04:31 +0500

    Улучшен алгоритм получения виджетов.

--- e-commerce-vue-js-app/src/services/utils/widgetsUtils.js
+++ e-commerce-vue-js-app/src/services/utils/widgetsUtils.js
@@ -1,10 +1,11 @@
-import { find as _find } from 'lodash'
+import { find as _find, filter as _filter } from 'lodash'
 
 export const getWidget = (widgets, key) => {
   if (widgets && key) {
-    const property = _find(Object.keys(widgets), (property) => property.toLowerCase().includes(key.toLowerCase()))
-    if (property) {
-      return JSON.parse(widgets[property])
+    const properties = _filter(Object.keys(widgets), (property) => property.toLowerCase().includes(key.toLowerCase() + '-'))
+    if (properties.length > 0) {
+      const property = _find(properties, (property) => widgets[property])
+      if (property) return JSON.parse(widgets[property])
     }
   }
   return null
